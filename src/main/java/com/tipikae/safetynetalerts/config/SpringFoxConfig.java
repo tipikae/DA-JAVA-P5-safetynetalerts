c742bf4f651480540e740dc1f65ce027683bf50a
tipikae <gilles.bd@free.fr>
2021-12-28 17:24:16 +0100

    adding javadoc syntax

--- src/main/java/com/tipikae/safetynetalerts/config/SpringFoxConfig.java
+++ src/main/java/com/tipikae/safetynetalerts/config/SpringFoxConfig.java
@@ -8,9 +8,19 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * A configuration class for Swagger2
+ * @author tipikae
+ * @version 1.0
+ *
+ */
 @Configuration
 public class SpringFoxConfig {   
-	
+
+	/**
+	 * Api method.
+	 * @return Docket
+	 */
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)
